63fff1b331bf74d1b2468d0ca242b60ac14248dd
Peter kanyori <peterynaenah23@gmail.com>
2025-11-26 18:29:37 +0300

    created the Home page and added its content

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Home() {
+return (
+<div className="">
+<h2 className="">Welcome to Tunestream</h2>
+<p className="">Your personalized music powered by Spotify</p>
+</div>
+)
+}
