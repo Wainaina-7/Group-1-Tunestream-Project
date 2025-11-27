fa564b63c563a3d8eacbc4d70cf7dd3c5f77b02a
Peter kanyori <peterynaenah23@gmail.com>
2025-11-27 13:31:56 +0300

    added heading and a button to connect to spotify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Login() {
   return (
     <div>
-      
+      <div className="">
+<h2 className="">Login with Spotify</h2>
+<button className="">Connect to Spotify</button>
+</div>
     </div>
   )
 }
