9de488bc55d4f172735f88437cb5618f895258b3
Peter kanyori <peterynaenah23@gmail.com>
2025-11-27 13:12:12 +0300

    added a heading and an input elemeent for searching tracks

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,15 @@
 export default function Search() {
 const [query, setQuery] = useState('')
 const [results, setResults] = useState([])
+return(
+    <div>
+        <h2 className="">Search Music</h2>
+<input
+className=""
+placeholder="Search tracks..."
+value={query}
+onChange={e => setQuery(e.target.value)}
+/>
+    </div>
+)
+}
