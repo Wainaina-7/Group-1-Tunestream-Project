b90e1c45d1ceddce51c894a239ce90a11566c9c7
Peter kanyori <peterynaenah23@gmail.com>
2025-11-27 13:22:38 +0300

    added a useEffect in Search page to add side effects when fetching data. Also added a dependency to rerender the app when values change

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import { searchTracks } from '../api/spotify'
+import SongCard from '../components/SongCard'
+
 export default function Search() {
 const [query, setQuery] = useState('')
 const [results, setResults] = useState([])
+
+useEffect(() => {
+if (query.length < 2) return
+searchTracks(query).then(tracks => setResults(tracks))
+}, [query])
 return(
     <div>
         <h2 className="">Search Music</h2>
