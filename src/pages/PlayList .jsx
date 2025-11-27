517977840601344208dfb712ebba55449705bdf1
Peter kanyori <peterynaenah23@gmail.com>
2025-11-27 13:26:48 +0300

    added a heading and a map function to map through the various songs

--- src/pages/PlayList .jsx
+++ src/pages/PlayList .jsx
@@ -4,7 +4,10 @@ export default function PlayList () {
     const [list, setList] = useState([])
   return (
     <div>
-      
+      <h2 className="">Your Playlists</h2>
+<div className="">
+{list.map(pl => <PlaylistCard key={pl.id} playlist={pl} />)}
+</div>
     </div>
   )
 }
