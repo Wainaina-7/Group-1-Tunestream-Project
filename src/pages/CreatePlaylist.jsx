55aaf4001637e0cd7387dd155d079caf1c986022
Peter kanyori <peterynaenah23@gmail.com>
2025-11-28 11:08:43 +0300

    added useState react hook and setter functions to change state wgen creating playlists

--- src/pages/CreatePlaylist.jsx
+++ src/pages/CreatePlaylist.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function CreatePlaylist() {
+const [name, setName] = useState('')
+const [desc, setDesc] = useState('')
+const [status, setStatus] = useState('')
   return (
     <div>
       
