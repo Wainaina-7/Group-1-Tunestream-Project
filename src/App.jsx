5e8652a57f676a851ee73dd83bf6bb0699f58d12
Peter kanyori <peterynaenah23@gmail.com>
2025-11-28 11:59:13 +0300

    imported the pages and other components to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Playlists from './pages/Playlists'
+import CreatePlaylist from './pages/CreatePlaylist'
+import Login from './pages/Login'
+
 export default function App() {
 return (
 <div className="">
