ba1a4dc83b09a407b2200610613bdbf0eb3647d1
Peter kanyori <peterynaenah23@gmail.com>
2025-11-28 11:11:25 +0300

    added a variable that contain spotify id

--- src/pages/CreatePlaylist.jsx
+++ src/pages/CreatePlaylist.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { createPlaylist } from '../api/spotify'
 export default function CreatePlaylist() {
 const [name, setName] = useState('')
 const [desc, setDesc] = useState('')
 const [status, setStatus] = useState('')
+const userId = localStorage.getItem('spotify_user_id')
   return (
     <div>
       
