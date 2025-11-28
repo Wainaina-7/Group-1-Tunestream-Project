cdb632da552534e45977dc5a6ccdd5c7187d3ab4
Peter kanyori <peterynaenah23@gmail.com>
2025-11-28 11:43:56 +0300

    created route links to the different pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,18 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div>
-      
+      <header className="">
+<nav className="">
+<h1 className="">Tunestream</h1>
+<ul className="">
+<li><Link className="" to="/">Home</Link></li>
+<li><Link className="" to="/search">Search</Link></li>
+<li><Link className="" to="/playlists">Playlists</Link></li>
+<li><Link className="" to="/create">Create</Link></li>
+<li><Link className="" to="/login">Login</Link></li>
+</ul>
+</nav>
+</header>
     </div>
   )
 }
